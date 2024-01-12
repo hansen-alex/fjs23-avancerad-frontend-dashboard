1f23608f8408345248c3da5a3d9ae0983e9cd08b
Alex Hansen <hansen.alex@telia.com>
2024-01-12 17:33:06 +0100

    style: icons

--- js/timeAndDate.js
+++ js/timeAndDate.js
@@ -4,7 +4,7 @@ const dateText = document.querySelector(".date-and-time .date");
 const updateDateAndTime = () => {
   const currentDateAndTime = new Date();
 
-  timeText.textContent = `${currentDateAndTime.getHours()}:${currentDateAndTime.getMinutes()}`;
+  timeText.textContent = `${currentDateAndTime.getHours()}:${currentDateAndTime.getMinutes()}`; //BUG: 19:3 at 19:03, probably same for hours
   dateText.textContent = `${currentDateAndTime.getDate()} ${currentDateAndTime.toLocaleString(
     "default",
     { month: "long" }
