19d784ef7e1cb939bc65914e5e1f9f32e3c56716
Alex Hansen <hansen.alex@telia.com>
2024-01-12 21:12:56 +0100

    feat: changing theme placeholder text from list. current bug if user leaves tab inactive

--- js/backgroundTheme.js
+++ js/backgroundTheme.js
@@ -47,3 +47,70 @@ const setBackground = async (event) => {
 backgroundThemeSubmitButton.addEventListener("click", setBackground);
 
 // setRandomBackground(); //Max 50 requests per hour so disabled for now
+
+const placeholderSuggestions = [
+  "SAAB",
+  "Cars",
+  "Boats",
+  "Airplanes",
+  "Space",
+  "Earth",
+  "Mars",
+  "Venus",
+  "Jupiter",
+  "Pluto",
+  "Forest",
+  "Desert",
+  "Ocean",
+  "Lake",
+  "River",
+  "Food",
+  "Animals",
+  "Dogs",
+  "Cats",
+  "Birds",
+  "Horses",
+];
+
+const removePlaceholderLetters = async (placeholder, letterDelay) => {
+  backgroundThemeInput.placeholder = placeholder.slice(0, -1);
+
+  if (backgroundThemeInput.placeholder.length > 0) {
+    await new Promise((res) => setTimeout(res, letterDelay));
+    await removePlaceholderLetters(
+      backgroundThemeInput.placeholder,
+      letterDelay
+    );
+  }
+};
+
+const addPlaceholderLetter = async (placeholder, letterDelay) => {
+  for (let i = 0; i < placeholder.length; i++) {
+    await new Promise((res) => setTimeout(res, letterDelay));
+    backgroundThemeInput.placeholder += placeholder[i];
+  }
+};
+
+//BUG: Workers are needed as setInterval and setTimeout do not work in inactive tabs, which breaks this
+const placeholderIntervalDelay = 5000; //Set time between placeholders
+const letterDelay = 300;
+let dynamicPlaceholderIntervalDelay = 0;
+const updatePlaceholder = async () => {
+  const newPlaceholder =
+    placeholderSuggestions[
+      Math.floor(Math.random() * placeholderSuggestions.length)
+    ];
+
+  const updateOperationTime =
+    backgroundThemeInput.placeholder.length * letterDelay +
+    newPlaceholder.length * letterDelay;
+  dynamicPlaceholderIntervalDelay =
+    placeholderIntervalDelay + updateOperationTime;
+
+  await removePlaceholderLetters(backgroundThemeInput.placeholder, letterDelay);
+  backgroundThemeInput.placeholder = "";
+  await addPlaceholderLetter(newPlaceholder, letterDelay);
+};
+
+updatePlaceholder();
+setInterval(updatePlaceholder, dynamicPlaceholderIntervalDelay);
